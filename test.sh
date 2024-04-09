f7614864d4c6884ea86f69985d4c0f581549739e
yogeshtemporal <yogesh.chouk@temporal.io>
2024-04-09 14:14:00 +1000

    Dockerfile changes

--- test.sh
+++
@@ -1,5 +0,0 @@
-temporal schedule list --address yogesh.a2dd6.tmprl.cloud:7233 --namespace yogesh.a2dd6  --tls-cert-path  /Users/yogeshchouk/cert/yo.pem --tls-key-path  /Users/yogeshchouk/cert/yo.key | awk '{print $1}' | grep "asd*" | while IFS= read -r schedule_id; do
-
-    temporal schedule delete -schedule-id "$schedule_id" --address yogesh.a2dd6.tmprl.cloud:7233 --namespace yogesh.a2dd6  --tls-cert-path  /Users/yogeshchouk/cert/yo.pem --tls-key-path  /Users/yogeshchouk/cert/yo.key
-sleep 2
-done
